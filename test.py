2e14942b499a384dea42af8912cf8b14f342ebca
Sumeyye <smisskender6155@gmail.com>
2026-05-12 23:07:44 +0300

    test

--- test.py
+++ test.py
@@ -6,14 +6,33 @@ BAUD = 9600
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
-# GT-521F52 Open komutu
-open_cmd = bytes.fromhex("55 AA 01 00 01 00 00 00 01 00 02 01")
+def send_packet(cmd, param=0):
+    device_id = 1
 
-ser.write(open_cmd)
-time.sleep(0.5)
+    packet = bytearray()
+    packet += b'\x55\xAA'
+    packet += device_id.to_bytes(2, 'little')
+    packet += param.to_bytes(4, 'little')
+    packet += cmd.to_bytes(2, 'little')
 
-response = ser.read(64)
+    checksum = sum(packet) & 0xFFFF
+    packet += checksum.to_bytes(2, 'little')
 
-print("Cevap:", response.hex(" "))
+    ser.write(packet)
+    time.sleep(0.2)
+    resp = ser.read(64)
+    print(f"CMD {cmd:#04x} cevap:", resp.hex(" "))
+    return resp
+
+# Open
+send_packet(0x0001)
+
+# CMOS LED ON
+send_packet(0x0012, 1)
+
+time.sleep(5)
+
+# CMOS LED OFF
+send_packet(0x0012, 0)
 
 ser.close()
